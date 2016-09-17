d242a9d21d3d8fc77178b7270f702bab20e71b80
Jonas Herskind Sejr <jonas.herskind.sejr@LEGO.com>
2016-09-17 12:29:52 +0200

    linear regression gradient descent works I think

--- Python/src/Handin 1/logisticregression.py
+++ Python/src/Handin 1/logisticregression.py
@@ -1,14 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy._erfa.core import cal2jd
+from mpmath.tests.test_linalg import improve_solution
 
-def load_data():
+
+def load_data(filename):
     """Loads the training data from file"""
     # Change this to the AU digits data set when it has been released!
-    train_file = np.load('mnistTrain.npz')
+    train_file = np.load(filename)
     images = train_file['images']
     labels = np.squeeze(train_file['labels'])  # TODO remove squeeze
     print('Shape of input data: %s %s' % (images.shape, labels.shape))
     return images, labels
+
+def load_data_27_only(filename):
+    digits, labels = load_data(filename)
+    idx1 = (labels == 2)
+    idx2 = (labels == 7)
+    print(idx1.shape)
+    img27 = digits[idx1 | idx2,:]
+    lab27 = labels[idx1 | idx2]
+    lab27[lab27==2] = 0
+    lab27[lab27==7] = 1
+    return img27, lab27
+
+def load_test_data_27_only():
+    return load_data_27_only('mnistTest.npz')
+
+def load_train_data_27_only():
+    return load_data_27_only('mnistTrain.npz')
+
 
 def logistic(z):
     """ 
@@ -36,10 +57,17 @@ def log_cost(X, y, w, reg=0):
         cost: Average Negative Log Likelihood of w
         grad: The gradient of the average Negative Log Likelihood at w
     """
-    #Use filtering
-    cost = -(np.sum(y.T*np.log(logistic(w*X.T))) + np.sum((1 - y.T)*np.log(1 - logistic(w*X.T))))
-    print('Please implement me without (too many) for-loops. I am important!')
-    grad = 0
+
+    #Jonas: Use filtering to optimize. In the below all calculations are done twice
+    linear_terms = np.dot(X, w)
+    logistic_terms = logistic(linear_terms)
+    positive_errors = np.multiply(np.log(logistic_terms), y)
+    negative_errors = np.multiply(np.log(1 - logistic_terms), (1 - y))
+    all_errors = np.concatenate((positive_errors, negative_errors))
+    cost = -(np.sum(all_errors)/np.shape(X)[0])
+
+    grad = -np.dot(X.T,(y - logistic_terms))
+
     return cost, grad
 
 def batch_grad_descent(X, y, w=None, reg=0):
@@ -55,9 +83,22 @@ def batch_grad_descent(X, y, w=None, reg=0):
         Learned weight vector (d x 1)
     """
     print('Please implement me and use all data in each gradient computation')
+    # Ensure arrays are 2d
+    if len(y.shape) == 1:
+        y = y.reshape(len(y), 1)
+
     if w is None:
-        w = np.zeros(X.shape[1])
-    w = X[0, :]
+        w = np.zeros(X.shape[1]).reshape(X.shape[1],1)
+    minimum_improvement = 0.00000000001
+    speed = 100
+    previous_cost = 1000000 #something very high
+    keeponimproving = True
+    while keeponimproving:
+        cost, grad = log_cost(X, y, w)
+        improvement = previous_cost - cost
+        w = w + speed*grad
+        print(w)
+        keeponimproving = improvement < 0 or improvement > minimum_improvement
     return w
 
 def mini_batch_grad_descent(X, y, w=None, reg=0, batchsize=16, epochs=10):
@@ -81,21 +122,23 @@ def mini_batch_grad_descent(X, y, w=None, reg=0, batchsize=16, epochs=10):
     n = X.shape[0]
     return w
 
+def execute_LGMobel(w, x, limit):
+    dotproduct = np.dot(x, w)
+    logi = logistic(dotproduct)
+    return 1 if logi > limit else 0
+
 def main():
     """An example main method. You should replace this with whatever you need."""
-    print('You rang sir/miss')
-    digits, labels = load_data()
-    idx1 = (labels == 2)
-    idx2 = (labels == 7)
-    print(idx1.shape)
-    img27 = digits[idx1 | idx2,:]
-    lab27 = labels[idx1 | idx2]
-    lab27[lab27==2] = 0
-    lab27[lab27==7] = 1
-
-    w = batch_grad_descent(img27, lab27, 0)
-    plt.imshow(w.reshape(28, -1, order='F'), cmap='bone')
-    print('Look look it is a pretty two i think')
+    train_img27, train_lab27 = load_train_data_27_only()
+    test_img27, test_lab27 = load_test_data_27_only()
+    w = batch_grad_descent(train_img27, train_lab27)
+    calc_labels = np.apply_along_axis(lambda x: execute_LGMobel(w, x, 0.5), 1, test_img27)
+    errors = (calc_labels == test_lab27)
+    print("Percent positive = " + str(np.mean(test_lab27)))
+    print("Error ration = " + str(np.mean(errors)))
+    #plt.imshow(w.reshape(28, -1, order='F'), cmap='bone')
+    #print('Look look it is a pretty two i think')
 
 if __name__ == '__main__':
     main()
+
--- Python/tests/Handin 1/test_logisticregression.py
+++ Python/tests/Handin 1/test_logisticregression.py
@@ -1,24 +1,81 @@
 import unittest
 import numpy as np
+from mpmath import limit
+
 from logisticregression import log_cost
-class MyTestCase(unittest.TestCase):
-    def test_single_positiv_column(self):
-        X = np.matrix([[1, 1]])
-        y = np.array([1])
-        w = np.array([3, 4])
+from logisticregression import batch_grad_descent
+from logisticregression import execute_LGMobel
+
+#Remember nosetests should be executed with --nocapture to see output
+class Log_CostTestCase(unittest.TestCase):
+    def test_cost_single_positiv_column(self):
+        X = np.array([[1, 1]])
+        y = np.array([[1]])
+        w = np.array([[3], [4]])
         cost, grad = log_cost(X, y, w, reg=0)
         np.testing.assert_almost_equal(cost, 0.00091146645377420212, 13)
 
-    def test_single_negative_column(self):
-        X = np.matrix([[1, 1]])
-        y = np.array([0])
-        w = np.array([3, 4])
+    def test_cost_single_negative_column(self):
+        X = np.array([[2, 2]])
+        y = np.array([[0]])
+        w = np.array([[3], [4]])
         cost, grad = log_cost(X, y, w, reg=0)
-        np.testing.assert_almost_equal(cost, 7.0009114664538208, 13)
+        np.testing.assert_almost_equal(cost, 14.000000831404552, 13)
 
-    def test_double_column(self):
-        X = np.matrix([[1, 1], [2, 2]])
-        y = np.array([0, 1])
+    def test_cost_double_column(self):
+        X = np.array([[1, 1], [2, 2]])
+        y = np.array([[1], [0]])
+        w = np.array([[3], [4]])
+        cost, grad = log_cost(X, y, w, reg=0)
+        np.testing.assert_almost_equal(cost, 7.000456148929163, 13)
+
+    def test_gradient__with_one_dimensional_arrays(self):
+        X = np.array([[1, 1], [2, 2], [3, 3]])
+        y = np.array([1, 0, 1])
         w = np.array([3, 4])
         cost, grad = log_cost(X, y, w, reg=0)
-        np.testing.assert_almost_equal(cost, 14.002734399361415, 13)
+        np.testing.assert_almost_equal(grad[0], 1.9990872834747755, 13)
+        np.testing.assert_almost_equal(grad[0], 1.9990872834747755, 13)
+
+    def test_gradient_column(self):
+        X = np.array([[1, 1], [2, 2], [3, 3]])
+        y = np.array([[1], [0], [1]])
+        w = np.array([[3], [4]])
+        cost, grad = log_cost(X, y, w, reg=0)
+        np.testing.assert_almost_equal(grad[0], 1.9990872834747755, 13)
+        np.testing.assert_almost_equal(grad[0], 1.9990872834747755, 13)
+        #-(1* (1 - (1/(1 + np.exp(-7)))) + 2* (0 - (1/(1 + np.exp(-14)))) + 3* (1 - (1/(1 + np.exp(-21)))))
+
+class Batch_grad_descentTestCase(unittest.TestCase):
+    def test_4_points_on_line(self):
+        X = np.array([[1, 1, 1], [1, 2, 2], [1, 3, 3], [1, 4, 4], [1, 2, 0], [1, 0, 2]])
+        y = np.array([[1], [1], [0], [0], [1], [0]])
+        w = batch_grad_descent(X,y)
+        np.testing.assert_equal(actual=len(w.shape), desired=2)
+        print(w)
+        np.testing.assert_equal(actual=w.shape[0], desired=X.shape[1])
+        np.testing.assert_equal(actual=w.shape[1], desired=1)
+
+    def test_4_points_on_line_with_onedimension_y(self):
+        X = np.array([[1, 1, 1], [1, 2, 2], [1, 3, 3], [1, 4, 4], [1, 2, 0], [1, 0, 2]])
+        y = np.array([1, 1, 0, 0, 1, 0])
+        w = batch_grad_descent(X, y)
+        np.testing.assert_equal(actual=len(w.shape), desired=2)
+        print(w)
+        np.testing.assert_equal(actual=w.shape[0], desired=X.shape[1])
+        np.testing.assert_equal(actual=w.shape[1], desired=1)
+
+class execute_LGMobelTestCase(unittest.TestCase):
+    def test_model_execution_positive(self):
+        w = np.array([1, 2, 3])
+        x = np.array([1, 1, 1])
+        limit = 0.5
+        np.testing.assert_equal(execute_LGMobel(w, x, limit), desired=1)
+        #1/(1 + np.exp(-6))
+
+    def test_model_execution_negative(self):
+        w = np.array([1, 2, 3])
+        x = np.array([1, -1, -1])
+        limit = 0.5
+        np.testing.assert_equal(execute_LGMobel(w, x, limit), desired=0)
+        #1/(1 + np.exp(4))
